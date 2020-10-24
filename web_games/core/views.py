56fceb8270fb073ba3372fb0d011d47e9645e14d
Sebastián Araya <arayariquelmes@gmail.com>
2020-10-23 21:26:12 -0300

    Generar interfaz de productos
    Generar las url de productos
    Agregar como dependencia humanize para formatear precios
    Agregar condiciones if en las herramientas de templating
    Implementar Font awesome
    Generar la condicion de pagina activa mediante request.path

--- web_games/core/views.py
+++ web_games/core/views.py
@@ -2,11 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    productos = []
-    for i in range(1,10):
-        prod = {"nombre": "producto " + str(i), "precio": i * 1000}
-        productos.append(prod)
-    return render(request, "index.html", {'titulo': 'Nuestros productos son lo mejor ya tu sabes', 'productos':productos})
+
+    return render(request, "index.html", {'titulo': 'Nuestros productos son lo mejor ya tu sabes'})
 
 def contacto(request):
     return render(request,"contacto.html")
--- web_games/productos/views.py
+++ web_games/productos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Producto
 # Create your views here.
+
+def verProductos(request):
+    productos = Producto.objects.all()
+    return render(request, "productos.html", {"productos":productos})
